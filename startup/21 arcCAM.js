05a3e4ce002c6f2adf46ebad80142c2257e2b4e3
Len Shelton <len@probotix.com>
2024-09-22 09:53:44 -0500

    renamed files

--- startup/21 arcCAM.js
+++ startup/21 arcCAM.js
@@ -1,7 +1,8 @@
-var title = "arcCAM";
+var title = "ArcCAM";
 var title2 = "OrderCurves";
+var title3 = "PointCoord";
 var html='<moi:Spacer/>';
-//html += '<moi:Spacer/><moi:CommandButton icon="icons/CPlaneIcon.png" onbuttonclick=""><moi:Text textid="arcCAM"/></moi:CommandButton>';
-//moi.ui.commandBar.CommandBarCustomUI.insertAdjacentHTML( "beforeEnd", html);
+
 moi.ui.commandBar.CommandBarCustomUI.insertAdjacentHTML( "beforeEnd", '<moi:Spacer/><moi:Spacer/><moi:CommandButton icon="icons/arcCAMIcon.png" command="arcCAM">'+title+'</moi:/andButton>');
 moi.ui.commandBar.CommandBarCustomUI.insertAdjacentHTML( "beforeEnd", '<moi:Spacer/><moi:CommandButton icon="icons/OrderIcon.png" command="OrderCurves">'+title2+'</moi:/andButton>');
+moi.ui.commandBar.CommandBarCustomUI.insertAdjacentHTML( "beforeEnd", '<moi:Spacer/><moi:CommandButton icon="icons/PointIcon.png" command="_PointCoord">'+title3+'</moi:/andButton>');
