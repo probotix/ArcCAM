bd65279a66bbba0026be9e99a8c83019a88c17a4
Len Shelton <len@probotix.com>
2024-09-27 09:56:07 -0500

    misc updates

--- startup/21 arcCAM.js
+++ startup/21 arcCAM.js
@@ -8,6 +8,6 @@ var html='<moi:Spacer/>';
 moi.ui.commandBar.CommandBarCustomUI.insertAdjacentHTML( "beforeEnd", '<moi:Spacer/><moi:Spacer/><moi:CommandButton icon="icons/arcCAMIcon.png" command="arcCAM">ArcCAM</moi:/andButton>');
 moi.ui.commandBar.CommandBarCustomUI.insertAdjacentHTML( "beforeEnd", '<moi:Spacer/><moi:CommandButton icon="icons/SetCurveStartIcon.png" command="SetCurveStart">SetCurveStart</moi:/andButton>');
 moi.ui.commandBar.CommandBarCustomUI.insertAdjacentHTML( "beforeEnd", '<moi:Spacer/><moi:CommandButton icon="icons/OrderIcon.png" command="OrderCurves">OrderCurves</moi:/andButton>');
-moi.ui.commandBar.CommandBarCustomUI.insertAdjacentHTML( "beforeEnd", '<moi:Spacer/><moi:CommandButton icon="icons/FixCirclesIcon.png" command="SetCircles">SetCircles</moi:/andButton>');
-moi.ui.commandBar.CommandBarCustomUI.insertAdjacentHTML( "beforeEnd", '<moi:Spacer/><moi:CommandButton icon="icons/FixCirclesIcon.png" command="RepairCircles">RepairCircles</moi:/andButton>');
+moi.ui.commandBar.CommandBarCustomUI.insertAdjacentHTML( "beforeEnd", '<moi:Spacer/><moi:CommandButton icon="icons/SetCirclesIcon.png" command="SetCircles">SetCircles</moi:/andButton>');
+moi.ui.commandBar.CommandBarCustomUI.insertAdjacentHTML( "beforeEnd", '<moi:Spacer/><moi:CommandButton icon="icons/RepairCirclesIcon.png" command="RepairCircles">RepairCircles</moi:/andButton>');
 moi.ui.commandBar.CommandBarCustomUI.insertAdjacentHTML( "beforeEnd", '<moi:Spacer/><moi:CommandButton icon="icons/PointIcon.png" command="_PointCoord">PointCoord</moi:/andButton>');
